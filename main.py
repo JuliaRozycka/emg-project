38eb0ac8cb4a8ba87fdae9635d55f82520331392
Julia <254756@student.pwr.edu.pl>
2023-05-17 17:55:33 +0200

    Added Utils

--- main.py
+++ main.py
@@ -18,39 +18,32 @@ def print_hi(name):  # Use a breakpoint in the code line below to debug your scr
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    cred = credentials.Certificate("emg-signals-firebase-adminsdk-q9auf-cc504f173c.json")
-    firebase_admin.initialize_app(cred, {'storageBucket': 'emg-signals.appspot.com'})
-    bucket = storage.bucket()
-    blob = bucket.blob("o2/osoba_2_lewa_p2.csv")
-    x = blob.download_to_filename('dane_testowe.csv')
+    # print_hi('PyCharm')
+    # cred = credentials.Certificate("emg-signals-firebase-adminsdk-q9auf-9f867ef3d8.json")
+    # firebase_admin.initialize_app(cred, {'storageBucket': 'emg-signals.appspot.com'})
+    # bucket = storage.bucket()
+    # blob = bucket.blob("o2/osoba_2_lewa_p2.csv")
+    # x = blob.download_to_filename('dane_testowe.csv')
 
     column_names = ["Biceps", "Triceps", "Zginacz", "Prostownik"]
-    df = pd.read_csv('dane_testowe.csv')
+    df = pd.read_csv('dane_testowe.csv', names=column_names, header=None)
     rows = len(df)
-    time_col = np.arange(0.001, rows * 0.001, 0.001)
+    time_col = np.arange(0.001, rows * 0.001 + 0.001 , 0.001)
 
     df['Czas'] = time_col
-    df = df.drop(df.index[-1])
+    df['Sum'] = df[column_names].sum(axis=1)
 
-
-    print(type(df["Czas"].iloc[0]))
-    df['Biceps'] = df['Biceps'].astype(float)
-    df['Triceps'] = df['Triceps'].astype(float)
-    df['Prostownik'] = df['Prostownik'].astype(float)
-    df['Zginacz'] = df['Zginacz'].astype(float)
-
-    #df['sum'] = df.sum(axis=1)
-
-    print(df.head())
+    print(df)
 
     #
     # # Add title and axis labels
-    # plt.title('Line Plot')
-    # plt.xlabel('Czas')
-    # plt.ylabel('Napięcie')
+    print(-0.0006103515625 + 0.001220703125 - 0.00091552734375 -0.00091552734375)
+
+    plt.title('Line Plot')
+    plt.xlabel('Czas')
+    plt.ylabel('Napięcie')
 
     # Show the plot
-    #plt.show()
+    plt.show()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
