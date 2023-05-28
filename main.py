2a211401e4d4845d35f63601739cee0828f0d8e6
Patrycja Sekula <psekula16@gmail.com>
2023-05-28 12:44:15 +0200

    Data extraction, movements segmentation for o1-o11 exept o2 - p4-p6

--- main.py
+++ main.py
@@ -7,17 +7,14 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     window_size = 1000  # Adjust the window size based on your needs
-    threshold = 0.0019 # Adjust the threshold factor based on your needs
+    threshold = 0.012 # Adjust the threshold factor based on your needs
     cutoff = 100  # Cutoff value in Hz
 
-
-
     # tutaj zmieniać tylko nazwę pliku
-    nazwa_pliku = 'osoba_7_lewa_p3.csv'
-
+    nazwa_pliku = 'osoba_2_prawa_p1.csv'
     data = read_data(f'raw_signals/{nazwa_pliku}', cutoff)
 
-    hand_movements = threshold_segmentation_with_window(data, threshold, window_size,ignore_after=6000)
+    hand_movements = threshold_segmentation_with_window(data, threshold, window_size,ignore_after=9000)
     print(len(hand_movements))
     print(hand_movements)
 
@@ -27,12 +24,9 @@ if __name__ == '__main__':
         "cutoff": cutoff,
         "segments": hand_movements
     }
-
     visualize_selected_moves(data, hand_movements)
-
-
     # zapisywanie od plików, TODO: tutaj należy zmieniać osobę i pomiar
-    #save_segments_to_files(7, 3, data, hand_movements, metadata, savefig=True)
+    #save_segments_to_files(2, 4, data, hand_movements, metadata, savefig=True)
 
 
     window_size = 100  # window size in samples
