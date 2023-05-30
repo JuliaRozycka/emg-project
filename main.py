ebc383b4c693eff2bf4dd12207322166b3a4a2fd
Patrycja Sekula <psekula16@gmail.com>
2023-05-30 14:30:33 +0200

    No changes made

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     window_size = 1000  # Adjust the window size based on your needs
-    threshold = 0.012 # Adjust the threshold factor based on your needs
+    threshold = 0.002 # Adjust the threshold factor based on your needs
     cutoff = 100  # Cutoff value in Hz
 
     # tutaj zmieniać tylko nazwę pliku
     nazwa_pliku = 'osoba_2_prawa_p1.csv'
     data = read_data(f'raw_signals/{nazwa_pliku}', cutoff)
 
-    hand_movements = threshold_segmentation_with_window(data, threshold, window_size,ignore_after=9000)
+    hand_movements = threshold_segmentation_with_window(data, threshold, window_size,ignore_after=5000)
     print(len(hand_movements))
     print(hand_movements)
 
