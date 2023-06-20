d842496b980af944355be9a325926c574d9ef57f
Julia <254756@student.pwr.edu.pl>
2023-06-20 17:37:50 +0200

    Added wykresy.txt with info, generated plots for statistics

--- Statistics.py
+++ Statistics.py
@@ -9,6 +9,8 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as lda
 from statsmodels.multivariate.manova import MANOVA
 
 from Classifiers import Classifiers
+from Metrics import Metrics
+
 
 def dekor(f):
     def wrapper(*args, **kwargs):
@@ -57,19 +59,27 @@ def normal_distribution_check(data: pd.DataFrame):
         isnormal = False
     return stats, p, isnormal
 
+@dekor
+def ttest_independent(a: Classifiers, b: Classifiers, metric: Metrics):
+    data_a = pd.read_csv(f'metrics/{a.value}_metrics.csv')
+    data_a = data_a[metric.name]
+    print(a.name, data_a.mean())
+    data_b = pd.read_csv(f'metrics/{b.value}_metrics.csv')
+    data_b = data_b[metric.name]
+    print(b.name, data_b.mean())
+    sns.kdeplot(data_a, fill=True, label=a.name)
+    sns.kdeplot(data_b, fill=True, label=b.name)
+    plt.title("Independent Sample T-Test")
+    plt.xlabel(metric.value)
+    plt.legend()
+    plt.show()
+    return stats.ttest_ind(data_a, data_b)
 
-def ttest_independent(a: Classifiers, b: Classifiers, metric: str):
-    a = pd.read_csv(f'metrics/{a.value}_metrics.csv')
-    a = a[metric]
-    print(a.name, a.mean())
-    b = pd.read_csv(f'metrics/{b.value}t_metrics.csv')
-    b = b[metric]
-    print(b.name, b.mean())
-    return stats.ttest_ind(a, b)
+
 
 
 @dekor
-def manova(metric_a: str, metric_b: str):
+def manova(metric_a: Metrics, metric_b: Metrics):
     data = []
 
     directories = ['metrics/knn_metrics.csv', 'metrics/svm_metrics.csv', 'metrics/dt_metrics.csv']
@@ -83,11 +93,11 @@ def manova(metric_a: str, metric_b: str):
 
         for _, row in df.iterrows():
             # Get the balanced accuracy and f1-score for each row
-            metric1 = row[metric_a]
-            metric2 = row[metric_b]
+            metric1 = row[metric_a.name]
+            metric2 = row[metric_b.name]
 
             # Create a new dictionary with the classifier name, balanced accuracy, and f1-score
-            data_row = {'Classifier': classifier_name, metric_a: metric1, metric_b: metric2}
+            data_row = {'Classifier': classifier_name, metric_a.name: metric1, metric_b.name: metric2}
 
             # Append the row to the data list
             data.append(data_row)
@@ -95,16 +105,16 @@ def manova(metric_a: str, metric_b: str):
     # Convert the data list to a DataFrame
     data = pd.DataFrame(data)
 
-    fit = MANOVA.from_formula(f'{metric_a} + {metric_b} ~ Classifier', data=data)
+    fit = MANOVA.from_formula(f'{metric_a.name} + {metric_b.name} ~ Classifier', data=data)
 
-    X = data[[metric_a, metric_b]]
+    X = data[[metric_a.name, metric_b.name]]
     y = data["Classifier"]
     post_hoc = lda().fit(X=X, y=y)
 
     # plot
-    X_new = pd.DataFrame(lda().fit(X=X, y=y).transform(X), columns=["lda1", "lda2"])
+    X_new = pd.DataFrame(lda().fit(X=X, y=y).transform(X), columns=[f"LDA ({metric_a.value})", f"LDA ({metric_b.value})"])
     X_new["Classifier"] = data["Classifier"]
-    sns.scatterplot(data=X_new, x="lda1", y="lda2", hue=data.Classifier.tolist())
+    sns.scatterplot(data=X_new, x=f"LDA ({metric_a.value})", y=f"LDA ({metric_b.value})", hue=data.Classifier.tolist())
     plt.show()
 
     return fit.mv_test()
@@ -152,16 +162,16 @@ def manova_all_metrics(a: Classifiers, b: Classifiers):
     return x
 
 @dekor
-def boxplot(classifier: Classifiers, metric: str):
+def boxplot(classifier: Classifiers, metric: Metrics):
     data = metrics_to_one_csv()
-    data = data[metric][(data['Classifier'] == classifier.value)]
+    data = data[metric.name][(data['Classifier'] == classifier.value)]
     column_data = list(data)
     matrix_data = np.array(column_data).reshape(5, 5)
     plt.boxplot(matrix_data.T, showmeans=True,
                 meanline=True)  # transpose the array to have iterations on x-axis and folds on y-axis
     plt.xlabel('Iteration')
-    plt.ylabel(metric.capitalize())
-    plt.title(f'{metric.capitalize()} distribution for {classifier.name} classifier')
+    plt.ylabel(metric.value.capitalize())
+    plt.title(f'{metric.value.capitalize()} distribution for {classifier.name} classifier')
     plt.xticks(np.arange(1, matrix_data.shape[0] + 1), labels=np.arange(1, matrix_data.shape[0] + 1))
     plt.show()
 
